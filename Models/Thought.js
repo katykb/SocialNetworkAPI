90eadab5759b9e326dadfce4f9a915f062af2913
Katy Brown <katybr35@gmail.com>
2022-07-07 10:24:15 -0500

    added Thoughts.js

--- Models/Thought.js
+++ Models/Thought.js
@@ -1,1 +1,32 @@
 const { Schema, Models } = require("mongoose");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+
+  createdAt: {
+    date: { type: Date, default: Date.now },
+  },
+
+  username: {
+    type: Schema.type.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  reactions: [reactionSchema],
+
+  toJSON: {
+    getters: true,
+  },
+  id: false,
+});
+
+const Thoughts = Models(Thoughts, thoughtSchema);
+
+module.exports = thoughtSchema;
