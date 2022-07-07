956fb5a57460d4063c1d7b7705ecf483126ccc3c
Katy Brown <katybr35@gmail.com>
2022-07-07 13:35:33 -0500

    updated Thought Controllers

--- routes/apiRoutes/thoughtRoutes.js
+++ routes/apiRoutes/thoughtRoutes.js
@@ -5,11 +5,21 @@ const {
   postNewThought,
   updateThoughtById,
   deleteThoughtById,
-
+  getReaction,
+  deleteReactionById,
 } = require("../controllers/usrController.js");
 
 router.route("/getThoughts").get(getThoughts).post(postNewThought);
 
-router.route("/:getSingleThought").get(getThoughtById).delete(deleteThoughtById).update(updateThoughtById);
+router
+  .route("/:getSingleThought")
+  .get(getThoughtById)
+  .delete(deleteThoughtById)
+  .update(updateThoughtById);
+
+router
+  .route("/getSingleThought/reactions")
+  .post(getReaction)
+  .delete(deleteReactionById);
 
 module.export = router;
